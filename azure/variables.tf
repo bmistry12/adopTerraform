9920f8535de929ad2c6bbeb30ecddd8614d278aa
bmistry12 <bhavina.h.mistry@accenture.com>
2018-10-23 23:05:06 +0100

    Update variables.tf

--- azure/variables.tf
+++ azure/variables.tf
@@ -24,11 +24,11 @@ variable "adop_username" {
 }
 
 variable "adop_password" {
-    default = "aDayToRemember_2"
+    default = ""
 }
 
 variable "public_key" { 
-    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCiXvnrtcWE5fEgdWhdXK+imTx2wDac2w/TjcncjqctfHRS+cytMUXTPKzajVkUpq3FOVDQkKHtxBLWCebU1QPGEs+kJKCtYoICBsG/7IpZvEo4R+xWuWh33CVLk9/8byJmsxxWJea9wXxKEUc6Wz7mVQ7AG2BvtkXW7+Tcvr5DdRWL3W1UuVmqoHlgud+bPLrfZNWf3jyAOR6qFd9z4PA9DxTxHrGeCrcxXyJeR/vLiLmGcZAiZEXIbXSdQLCZP22hpvsFfsH6jqUXGQ5RBeHnR6oWkOBEUIckxyjNQNDqFHfzW/sv7gC+fmb0bcJokgmx9VD7WRBZ/VMShFaycn59 bhav_terraform_example"  
+    default = ""  
 }
 
 
